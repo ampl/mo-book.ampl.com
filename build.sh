4045e9b4aae4a07c6a507ab659671e4ae5d7b607
Filipe Brandão <fdabrandao@gmail.com>
2023-12-27 16:33:03 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python -m sphinx -b html . _build/mo-book 
-# cd _build/
-# rsync -av mo-book/ kinsta-staging:~/public/mo-book
-# rsync -av mo-book/ kinsta:~/public/mo-book
+# rsync -av _build/mo-book/ kinsta-staging:~/public/mo-book
+# rsync -av _build/mo-book/ kinsta:~/public/mo-book
